26313da6bb2b3bbc9279747ee2762b7cd0317a0a
Huldar <huldarh@gmail.com>
2021-10-07 11:18:20 +0000

    Daemi 1

--- HD5.m
+++ HD5.m
@@ -5,6 +5,20 @@ disp("");
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp("Daemi 1");
 disp("");
+H = 10;
+hnutpunktar1 = [0 1 0;
+               0 0 H];
+hnutp_per_element = 3;
+nr_element = 1;
+N1 = formfallafylki(hnutp_per_element, nr_element, hnutpunktar);
+B1 = diff(N1);
+
+disp("a)");
+disp("");
+
+lausn1 = leysa_skifu(hnutpunktar1)
+
+
 
 % Daemi 2
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
