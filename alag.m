8eaca6c126f7b7836a2947295c96b2736c6e8024
saemi10 <saemundur10@gmail.com>
2021-10-11 14:39:42 +0000

    minor fixes

--- alag.m
+++ alag.m
@@ -11,9 +11,8 @@ gamma1 = x3 - x2; gamma2 = x1 - x3; gamma3 = x2 - x1;
 alpha1 = x2*y3 - x3*y2; alpha2 = x3*y1 - x1*y3; alpha3 = x1*y2 - x2*y1;
 A = (alpha1 + alpha2 + alpha3)/2;
 %  shape functions
-N = [1/(2*A)*(alpha1+beta1*x+gamma1*y)  1/(2*A)*(alpha2 + beta2*x+gamma2*y)  1/(2*A)*(alpha3+beta3*x+gamma3*y)]
+N = [1/(2*A)*(alpha1+beta1*x+gamma1*y)  1/(2*A)*(alpha2 + beta2*x+gamma2*y)  1/(2*A)*(alpha3+beta3*x+gamma3*y)];
 x = 0;
 evalN = eval(N);
 f = t*int(evalN.'*p,y,0,L);
 end
-
